ba1cbabc4fe28ebe122aa29f15750b4a728263a5
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2026-06-11 22:26:12 +0700

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        // vị trí, vận tốc
+        double[][] cars = new double[n][2];
+
+        for(int i = 0; i < n; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
+        }
+
+        // sắp xếp theo vị trí tăng dần, 
+        Arrays.sort(cars, (a, b) -> Double.compare(a[0], b[0]));
+
+        int fleets = 0;
+        // Thời gian của chiếc xe chậm nhất phía trước
+        double maxTime = 0;
+
+        for(int i = n - 1; i >= 0; i--){
+            double currentTime = cars[i][1];
+            if(currentTime > maxTime){
+                fleets++;
+                maxTime = currentTime;
+            }
+        }
+        return fleets;
+    }
+}
