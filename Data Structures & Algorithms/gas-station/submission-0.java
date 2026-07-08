078d66fe526aa9cce597dffbaf69a489de75cb28
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2026-07-08 10:40:40 +0700

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    // gas[i]: lượng gas tại trạm i
+    // cost[i]: lượng gas cần thiết để đi từ trạm i đến trạm i + 1
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int start = n -1, end = 0;
+        int tank = gas[start] - cost[start];
+        while(start > end){
+            if(tank < 0){
+                start--;
+                tank += gas[start] - cost[start];
+            }else{
+                tank += gas[end] - cost[end];
+                end++;
+            }
+        }
+
+        return tank >= 0 ? start : -1;
+    }
+}
